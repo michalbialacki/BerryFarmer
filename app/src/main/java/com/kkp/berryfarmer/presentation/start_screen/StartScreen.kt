db2197573a30aa7ef4cafb33274529a15e771b7e
Michal <michalbialacki@gmail.com>
2022-10-15 21:14:31 +0200

    15.10.2022 - Added logo :)

--- app/src/main/java/com/kkp/berryfarmer/presentation/start_screen/StartScreen.kt
+++ app/src/main/java/com/kkp/berryfarmer/presentation/start_screen/StartScreen.kt
@@ -2,6 +2,7 @@ package com.kkp.berryfarmer.presentation.start_screen
 
 import android.icu.text.SimpleDateFormat
 import android.util.Log
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -14,11 +15,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.ViewModel
 import androidx.navigation.NavController
+import com.kkp.berryfarmer.R
 import com.kkp.berryfarmer.presentation.start_screen.components.IconSurface
 import com.kkp.berryfarmer.ui.theme.BerryFarmerTheme
 import java.time.LocalDate
@@ -43,16 +47,12 @@ fun StartScreen(
             ),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Box(
-                modifier = Modifier
-                    .height(180.dp)
-                    .width(240.dp)
-                    .clip(
-                        RoundedCornerShape(10.dp)
-                    )
-                    .background(Color.LightGray)
-
-            ) // This is a placeholder for logo, change when logo created
+            Image(
+                painter = painterResource(id = R.drawable.logo_v1),
+                contentDescription ="",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.height(200.dp).width(240.dp)
+            )
             Spacer(modifier = Modifier
                 .fillMaxWidth()
                 .height(16.dp))
